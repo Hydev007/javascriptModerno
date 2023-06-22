1e126417494679b8cd28a913126c12ebecdd9d0a
Saul <SE38993@grupo.ypf.com>
2023-06-22 13:21:30 -0300

    booleans

---
+++ booleans3-app.js
@@ -0,0 +1,17 @@
+const autenticado = true;
+
+if(autenticado === true){
+    console.log("puede pasar")
+} else {
+    console.log("no puede pasar")
+}
+
+if(autenticado){
+    console.log("puede pasar")
+} else {
+    console.log("no puede pasar")
+}
+
+//operador ternario
+
+console.log(autenticado ? "puede pasar" : "no puede pasar");
